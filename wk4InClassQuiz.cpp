f29aaa6a9ee093ea1a522052c8be3f72e0233715
Dan Sajjad <dan.sajjad@gmail.com>
2018-09-17 21:35:15 -0700

    Vector Usage - integers in ascending order

--- wk4InClassQuiz.cpp
+++ wk4InClassQuiz.cpp
@@ -79,9 +79,10 @@ int Triangle::getArea() {
 
 
 
-/////////////////////////////////////////
-// Template To Compare 2 #s or Strings //
-/////////////////////////////////////////
+
+          /////////////////////////////////
+          // Template To Compare 2 Types //
+          /////////////////////////////////
 
 
 template <typename T, typename V>
@@ -89,6 +90,18 @@ bool isLessThan(const T& a, const V& b) {
   return a < b;
 }
 
+
+  ////////////////////////////////////////////////
+  // Vector Usage - integers in ascending order //
+  ////////////////////////////////////////////////
+
+bool is_ascending(vector<int> inputs) {
+  for (int i = 0; i < inputs.size() - 1; i++) {
+    if(inputs.at(i) > inputs.at(i + 1)) 
+      return false;
+  }
+  return true;
+ }
 
 // =============================== MAIN ========================
 
@@ -107,5 +120,12 @@ int main() {
   cout << isLessThan<char>('a', 'h') << endl;
   cout << isLessThan<string>("hat", "abb") << endl;
   cout << isLessThan<string>("abb", "hat") << endl;
+
+
+
+  vector<int> v = {7, 5, 16, 8};
+  vector<int> w = {7, 8, 9, 10};
+  cout << is_ascending(v) << endl;
+  cout << is_ascending(w) << endl;
 
 }
