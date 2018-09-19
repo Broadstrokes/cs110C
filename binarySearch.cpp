f26cd044856b87b4ae487e89c2c849de43f09c6b
Dan Sajjad <dan.sajjad@gmail.com>
2018-09-18 22:50:08 -0700

    Write swap function using references, pointers, templates

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -78,3 +78,92 @@ int getMidpoint(int start, int end) {
 
 
 
+
+
+    /////////////////////////
+    // SWAP WITH REFERENCE //
+    /////////////////////////
+void mySwapWithReference(int &a, int &b) {
+  int temp = a;
+  a = b;
+  b = temp;
+}
+
+
+    ////////////////////////
+    // SWAP WITH POINTERS //
+    ////////////////////////
+void mySwapWithPointers(int *a, int *b) {
+  int temp = *a; //declaring a pointer variable
+  *a = *b; 
+  *b = temp;
+}
+
+
+
+    ////////////////////////////////
+    //using regular swap function //
+    ////////////////////////////////
+void swap(int *int1, int *int2);
+
+    ////////////////////
+    //using templates //
+    ////////////////////
+template<typename T>
+T swapi(T &int1, T &int2){
+  T temp = int1;
+  int1 = int2;
+  int2 = temp;
+}
+
+
+int main() {
+  int int1 = 1;
+  int int2 = 2;
+
+  swap(int1, int2);
+  cout<< "1: " << int1 << " 2: " << int2 << endl;
+  
+  swapi(int1, int2);
+  cout<< "1: " << int1 << " 2: " << int2 << endl;
+
+  void swap(int *int1, int *int2) { //* here are pointers
+    int temp = *int1;  //here we are dereferencing to get value
+   *int1 = *int2;
+   *int2 = temp;
+  }
+
+}
+
+
+/*
+  Using template
+  template <typename T> 
+
+  T Swap (T &int1, T &int2) {
+    T temp = int1; 
+    int1 = int2; 
+    int2 = temp;
+  }
+*/
+
+
+    /////////////////////////
+    // READING FROM A FILE //
+    /////////////////////////
+
+ifstream fin("order.dat");
+const int SIZE=20;
+int q[SIZE];
+double price[SIZE];
+double total=0;
+
+if (fin.is_open()){
+  for (int i=0; i<6;i++){
+    fin >> q[i];
+    fin >> price[i];
+    total=total+(q[i]*price[i]);
+
+  }
+}
+cout << total;
