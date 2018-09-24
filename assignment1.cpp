1698d92305e0c01b4fa2c9c7e169048ed6b1579f
Dan Sajjad <dan.sajjad@gmail.com>
2018-09-23 22:57:02 -0700

    Add function to rotate array

--- assignment1.cpp
+++ assignment1.cpp
@@ -89,3 +89,25 @@ unsigned long long factorial(int num) {
     return result;
   }
 }
+
+
+void rotateLeft(int arr[], int numRotations, int arrSize) {
+  cout << "==========================" << endl;
+  cout << "Rotate" << endl; 
+  cout << "==========================" << endl;
+
+    for (int i = 0; i < numRotations; i++) {
+      int temp = arr[0];
+      int j;
+      for (j = 0; j < arrSize - 1; j++) {
+          arr[j] = arr[j + 1];
+      }
+      arr[j] = temp;
+    }
+}
+ 
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+      cout << arr[i] << " ";
+    }
+}
