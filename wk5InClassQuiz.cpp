35702e53cab75f9714934cf9e6ee06172d93a2a9
Dan Sajjad <dan.sajjad@gmail.com>
2018-09-24 20:01:20 -0700

    Calculate time using time.h

--- wk5InClassQuiz.cpp
+++ wk5InClassQuiz.cpp
@@ -1,21 +1,20 @@
 #include <algorithm>
 #include <iostream>
 #include <time.h> // C library
-#include <chrono> // C++ library
-using namespace std;
-using namespace std::chrono;
+
 
 unsigned int long long factorial(int num);
 void testTimeUsingTimeHLibrary(int num);
-void testTimeUsingChronoLibrary(int num);
 
 int main() {
   testTimeUsingTimeHLibrary(5);
-  testTimeUsingChronoLibrary(5);
   return 0;
 }
 
 
+/////////////////////////
+// Calculate factorial //
+/////////////////////////
 unsigned int long long factorial(int num) {
 	int result = 1;
 	
@@ -26,6 +25,9 @@ unsigned int long long factorial(int num) {
 	return result;
 }
 
+///////////////////////
+// Time using time.h //
+///////////////////////
 void testTimeUsingTimeHLibrary(int num) {
   // using time.h from C library
   clock_t start, end;
@@ -37,31 +39,4 @@ void testTimeUsingTimeHLibrary(int num) {
   cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC; // in seconds
 
   cout << cpu_time_used * 1000 << " milliseconds";
-}
-
-
-/*
-  For clean and robust C++ programs we should strive
-  to use C++ specific language constructs only.
-*/
-void testTimeUsingChronoLibrary(int num) {
-  // using chrono from C++ library
-
-  // Use auto keyword to avoid typing long 
-  // type definitions to get the timepoint 
-  // at this instant use function now() 
-  auto start = high_resolution_clock::now();
-  cout << "factorial(" << num << ") " << factorial(num) << endl;
-  auto stop = high_resolution_clock::now();
-
-
-  // Subtract stop and start timepoints and 
-  // cast it to required unit. Predefined units 
-  // are nanoseconds, microseconds, milliseconds, 
-  // seconds, minutes, hours. Use duration_cast() 
-  // function. 
-  auto duration = duration_cast<microseconds>(stop - start);
-
-   cout << "Time taken by function: "
-        << duration.count() * 0.001 << " milliseconds" << endl;
 }
