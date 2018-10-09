bd595522585b2ddc7e1483919769cc58b92ee9f7
Dan Sajjad <dan.sajjad@gmail.com>
2018-10-08 19:06:54 -0700

    Fibonacci sequence using recursion

--- week7InClassQuiz.cpp
+++ week7InClassQuiz.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
 
+int fib(int n);
+
+int main() {
+  cout << (fib(0) == 0)  << endl;
+  cout << (fib(1) == 1) << endl;
+  cout << (fib(2) == 1) << endl;
+  cout << (fib(3) == 2) << endl;
+  cout << (fib(4) == 3) << endl;
+  cout << (fib(5) == 5) << endl;
+  cout << (fib(10) == 55) << endl;
+  cout << (fib(15) == 610) << endl;
+  cout << (fib(20) == 6765) << endl;
+  cout << (fib(25) == 75025)<< endl;
+  cout << (fib(30) == 832040) << endl;
+}
+
+int fib(int n) {
+    if (n <= 2)
+      return 1;
+    else
+      return fib(n-1) + fib(n-2);
+}
