aaeda2d3238118de0ebedb1bbf4469596d38518a
Dan Sajjad <dan.sajjad@gmail.com>
2018-10-17 22:18:08 -0700

    isPalindrome recursive

--- hw2.cpp
+++ hw2.cpp
@@ -15,3 +15,15 @@ int GCD(int x, int y) {
    return x;
  return GCD(y, x % y);
 } 
+
+
+bool isPalindrome(string str) {
+	int start = 0;
+	int end = str.size() - 1;
+
+	if (start >= end)
+		return true;
+	else if (str[start] != str[end])
+		return false;
+	return isPalindrome(str.substr (1, str.size() -2));
+}
