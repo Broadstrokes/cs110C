944ea33146e978bd5e6ff5c8163392b96d68473e
Dan Sajjad <dan.sajjad@gmail.com>
2018-10-17 22:28:58 -0700

    to the nth power recursive

--- hw2.cpp
+++ hw2.cpp
@@ -4,13 +4,21 @@ using namespace std;
 
 int GCD(int x, int y);
 bool isPalindrome(string str);
-
+int powOfn(int n, double m);
 
 int main() {
 	cout << GCD(18, 24) << endl;	// 6
 	cout << GCD(42, 35) << endl;	// 7
 	cout << isPalindrome("madam") << endl;
 	cout << isPalindrome("maa") << endl;
+
+
+	cout << powOfn(2, 0) << endl;
+	cout << powOfn(2, 1) << endl;
+	cout << powOfn(2, 2) << endl;
+	cout << powOfn(2, 3) << endl;
+	cout << powOfn(2, 4) << endl;
+	cout << powOfn(2, 5) << endl;
 }
 
 
@@ -31,4 +39,12 @@ bool isPalindrome(string str) {
 	else if (str[start] != str[end])
 		return false;
 	return isPalindrome(str.substr (1, str.size() -2));
+}
+
+
+// Q14 Power of N
+int powOfn(int n, double m) {
+  if (m == 0)
+    return 1;
+  return n * powOfn(n, m - 1);
 }
