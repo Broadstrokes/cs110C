f453c21b7f57398d0008454b0f48061264210aaf
Dan Sajjad <dan.sajjad@gmail.com>
2018-10-17 22:27:37 -0700

    Clean up

--- hw2.cpp
+++ hw2.cpp
@@ -2,21 +2,26 @@
 #include <string>
 using namespace std;
 
+int GCD(int x, int y);
+bool isPalindrome(string str);
+
 
 int main() {
- cout << GCD(18, 24) << endl;	// 6
- cout << GCD(42, 35) << endl;	// 7 
+	cout << GCD(18, 24) << endl;	// 6
+	cout << GCD(42, 35) << endl;	// 7
+	cout << isPalindrome("madam") << endl;
+	cout << isPalindrome("maa") << endl;
 }
 
 
-//Q6 GCD recursive
+// Q6 GCD recursive
 int GCD(int x, int y) {
- if (y == 0)
-   return x;
- return GCD(y, x % y);
+	if (y == 0)
+		return x;
+	return GCD(y, x % y);
 } 
 
-
+// Q13 Palindrome recursive
 bool isPalindrome(string str) {
 	int start = 0;
 	int end = str.size() - 1;
