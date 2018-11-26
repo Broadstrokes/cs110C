75a8bc6affc00343a932955229c2d3faa98e49f4
Dan Sajjad <dan.sajjad@gmail.com>
2018-11-25 21:09:01 -0800

    Write an array based list implementation with various search methods

--- hw3.cpp
+++ hw3.cpp
@@ -139,3 +139,134 @@ public:
   // Return true if current position is at end of the list
   virtual bool isAtEnd() = 0;
 };
+
+
+/////////////////////////////////////
+// Array-based list implementation //
+/////////////////////////////////////
+template <typename E>
+class AList : public List<E> {
+private:
+  int maxSize; // Maximum size of list
+  int listSize; // Number of list items now
+  int curr; // Position of current element
+  E* listArray; // Array holding list elements
+  int recursiveBinarySearch(double arrayToSearch[], int low, int high, int tgt) {
+  	if (low <= high) {
+    		int middle = (low + high) / 2;
+				if (arrayToSearch[middle] == tgt) { return middle; }   
+				else if (arrayToSearch[middle] < tgt) { return recursiveBinarySearch(arrayToSearch, middle + 1, high, tgt); }
+        else { return recursiveBinarySearch(arrayToSearch, low, middle - 1, tgt); }
+ 		}
+
+   	return -1; 
+	}
+
+public:
+  AList(int size = 100) { // Constructor replace defaultSize (now 10) with the default you want otherwise leave empty
+    maxSize = size;
+    listSize = curr = 0;
+    listArray = new E[maxSize];
+  }
+  ~AList() { delete [] listArray; } // Destructor
+  void clear() { // Reinitialize the list
+    delete [] listArray; // Remove the array
+    listSize = curr = 0; // Reset the size
+    listArray = new E[maxSize]; // Recreate array
+  }
+  // Insert "it" at current position
+  void insert(const E& it) {
+    Assert(listSize < maxSize, "List capacity exceeded");
+    for(int i=listSize; i>curr; i--) // Shift elements up
+      listArray[i] = listArray[i-1]; // to make room
+    listArray[curr] = it;
+    listSize++; // Increment list size
+  }
+  void append(const E& it) { // Append "it"
+    Assert(listSize < maxSize, "List capacity exceeded");
+    listArray[listSize++] = it;
+  }
+  // Remove and return the current element.
+  E remove() {
+    Assert((curr>=0) && (curr < listSize), "No element");
+    E it = listArray[curr]; // Copy the element
+    for(int i=curr; i<listSize-1; i++) // Shift them down
+      listArray[i] = listArray[i+1];
+    listSize--; // Decrement size
+    return it;
+  }
+  void moveToStart() { curr = 0; } // Reset position
+  void moveToEnd() {  curr = listSize; } // Set at end
+  void prev() {  if (curr != 0) curr--; }  // Back up
+  void next() {  if (curr < listSize) curr++;}
+  int length() const { return listSize; } // Return list size
+  int currPos() const { return curr; }  // Return current position
+  // Set current list position to "pos"
+  void moveToPos(int pos) {
+    Assert ((pos>=0)&&(pos<=listSize), "Pos out of range");
+    curr = pos;
+  }
+  const E& getValue() const { // Return current element
+    Assert((curr>=0)&&(curr<listSize),"No current element");
+    return listArray[curr];
+  }
+  // Return true if current position is at end of the list
+  bool isAtEnd() { return curr == listSize; }
+
+  int ssearch(double tgt) { 
+  	int result = -1;
+
+		for (moveToStart(); !isAtEnd(); next()) {
+  		double it = getValue();
+  		if (it == tgt) {
+  			return currPos();
+  		}
+		}			
+
+  	return result;
+  }
+
+  int ibsearch(double tgt) {
+  	int result = -1;
+  	double arrayToSearch[length()];
+  	int n = sizeof(arrayToSearch)/sizeof(arrayToSearch[0]);
+  	
+		for (moveToStart(); !isAtEnd(); next()) {
+			arrayToSearch[currPos()] = getValue();
+		}
+
+  	sort(arrayToSearch, arrayToSearch + n); 
+
+  	int low = 0;
+  	int high = n - 1; 
+
+
+		while (low <= high) {
+			int middle = low + (high - low) / 2;
+
+			if (arrayToSearch[middle] == tgt) { return middle; } 
+			else if (arrayToSearch[middle] < tgt) { low = middle + 1; }
+			else { high = middle - 1; }
+		}
+  
+  	return result;
+  }
+
+
+  int rbsearch(double tgt) {
+  	double arrayToSearch[length()];
+  	int n = sizeof(arrayToSearch)/sizeof(arrayToSearch[0]);
+  	
+		for (moveToStart(); !isAtEnd(); next()) {
+			arrayToSearch[currPos()] = getValue();
+		}
+
+  	sort(arrayToSearch, arrayToSearch + n); 
+
+  	int low = 0;
+  	int high = n - 1;
+
+  	return recursiveBinarySearch(arrayToSearch, low, high, tgt);
+  }
+};
+
